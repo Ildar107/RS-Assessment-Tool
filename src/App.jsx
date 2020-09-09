8f3fbdeb45aea498732970e3598e1d8a7f1d654c
Ildar107 <ildar107wk@gmail.com>
2020-09-09 16:32:24 +0300

    build: add ant design lib

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {
   Route, Switch, Redirect,
 } from 'react-router-dom';
+import { DatePicker } from 'antd';
 import routes from './constants/routes';
 
 const App = () => (
   <Switch>
     <Route path={routes.LANDING} exact>
       <h1>Start Project!</h1>
+      <DatePicker />
     </Route>
     <Route path={routes.TEAM} exact>
       <h1>Dream Team</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 import routes from './constants/routes';
 import './assets/styles/main.scss';
+import 'antd/dist/antd.css';
 // import './assets/style/icons.min.css';
 
 ReactDOM.render(
