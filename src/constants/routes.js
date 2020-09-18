ad2056e39fd33e39f678885e81e28ec4f352d729
Alex <alex.gabriel@mail.ru>
2020-09-18 12:06:08 +0300

    feat: add routes and auth and home folders with files

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,13 @@
 const LANDING = '/';
 const TEAM = '/team';
+const TASKS = '/tasks';
+const REVIEWS = '/reviews';
+const REQUESTS = '/requests';
 
 export default {
   LANDING,
   TEAM,
+  TASKS,
+  REVIEWS,
+  REQUESTS,
 };
