29d26b807cbc8de13ddad36350cd8c97e6e92299
Alex <alex.gabriel@mail.ru>
2020-09-18 18:04:29 +0300

    feat: add logged user info

--- src/auth0-provider-with-history.jsx
+++ src/auth0-provider-with-history.jsx
@@ -3,9 +3,10 @@ import { useHistory } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
+  // const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const domain = 'dev-dj0o9orj.eu.auth0.com';
+  const clientId = 'k0cnyyG6FUX6ASMZPAEeDjhbGvO890Ia';
   const history = useHistory();
 
   const onRedirectCallback = (appState) => {
--- src/pages/home/MainPage.jsx
+++ src/pages/home/MainPage.jsx
@@ -4,9 +4,14 @@ import { useAuth0 } from '@auth0/auth0-react';
 import routes from '../../constants/routes';
 
 const MainPage = () => {
-  const { logout } = useAuth0();
+  const { logout, user } = useAuth0();
+  //  const { user } = useAuth0();
+  // const { nickname, picture } = user;
+  console.log(user);
   return (
     <div>
+      <h3>{user?.nickname}</h3>
+      <img src={user?.picture} alt="pic" />
       <ul>
         {/* <li><Link to={routes.MAINPAGE}><span>Main Page</span></Link></li> */}
         <li><Link to={routes.TASKS}><span>Tasks</span></Link></li>
