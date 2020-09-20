8be0fee8d7385cc720990b82672bb2eabd2c5c20
Alex <alex.gabriel@mail.ru>
2020-09-20 16:33:12 +0300

    fix: private routing errors

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,19 @@ import MainPage from './pages/home/MainPage';
 import AuthPage from './pages/authentication/AuthPage';
 import PrivateRoute from './components/PrivateRoute';
 
+// const Tasks = <h1>TASKS</h1>;
 const App = () => (
   <Switch>
-    <Route path={routes.AUTHENTICATION} exact>
-      <AuthPage />
-    </Route>
-    <PrivateRoute path={routes.LANDING} exact>
-      <MainPage />
-    </PrivateRoute>
-    <Route path={routes.TEAM} exact>
-      <h1>Dream Team</h1>
-    </Route>
+    <Route path={routes.AUTHENTICATION} exact component={AuthPage} />
+    <PrivateRoute path={routes.LANDING} exact component={MainPage} />
+    <Route path={routes.TEAM} exact component={() => <h1>Dream Team</h1>} />
     {/* <Route path={routes.MAINPAGE} exact>
       <h1>MAINPAGE</h1>
     </Route> */}
 
-    <PrivateRoute path={routes.TASKS} exact>
-      <h1>TASKS</h1>
-    </PrivateRoute>
-    <PrivateRoute path={routes.REVIEWS} exact>
-      <h1>REVIEWS</h1>
-    </PrivateRoute>
-    <PrivateRoute path={routes.REQUESTS} exact>
-      <h1>REQUESTS</h1>
-    </PrivateRoute>
+    <PrivateRoute path={routes.TASKS} exact component={() => <h1>TASKS</h1>} />
+    <PrivateRoute path={routes.REVIEWS} exact component={() => <h1>REVIEWS</h1>} />
+    <PrivateRoute path={routes.REQUESTS} exact component={() => <h1>REQUESTS</h1>} />
     <Route>
       <Redirect to={routes.LANDING} />
     </Route>
