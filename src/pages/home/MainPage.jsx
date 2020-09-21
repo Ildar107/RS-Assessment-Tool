f4f8bb8dac58dd3c53d6573521b656765505af90
Alex <alex.gabriel@mail.ru>
2020-09-21 11:45:44 +0300

    feat: add antd styling to menu

--- src/pages/home/MainPage.jsx
+++ src/pages/home/MainPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Card, Button } from 'antd';
+import { Card, Button, Menu } from 'antd';
+// import {
+//   PieChartOutlined,
+//   DesktopOutlined,
+//   ContainerOutlined,
+// } from '@ant-design/icons';
 import routes from '../../constants/routes';
+import './mainPage.scss';
 
 const { Meta } = Card;
 
@@ -14,26 +20,36 @@ const MainPage = ({ setIsAuthenticatedState, role }) => {
   console.log('user', user);
   return (
     <div className="main-page-wrapper">
-      {/* <h3>{user?.nickname || JSON.parse(localStorage.getItem('user'))?.nickname}</h3>
-      <h4>
-        Role:
-        {` ${role}`}
-      </h4>
-      <img src={user?.picture || JSON.parse(localStorage.getItem('user'))?.picture} alt="pic" /> */}
-      <Card
-        style={{ width: 300 }}
-        cover={<img src={user?.picture || JSON.parse(localStorage.getItem('user'))?.picture} alt="pic" />}
-      >
-        <Meta
-          title={user?.nickname || JSON.parse(localStorage.getItem('user'))?.nickname}
-          description={role}
-        />
-      </Card>
-      <ul>
-        {/* <li><Link to={routes.MAINPAGE}><span>Main Page</span></Link></li> */}
-        <li><Link to={routes.TASKS}><span>Tasks</span></Link></li>
-        <li><Link to={routes.REVIEWS}><span>Reviews</span></Link></li>
-        <li><Link to={routes.REQUESTS}><span>Review requests</span></Link></li>
+      <div className="user-info-and-menu-wrapper">
+        <Card
+          hoverable
+          style={{ width: 300 }}
+          cover={<img src={user?.picture || JSON.parse(localStorage.getItem('user'))?.picture} alt="pic" />}
+        >
+          <Meta
+            title={user?.nickname || JSON.parse(localStorage.getItem('user'))?.nickname}
+            description={role}
+          />
+        </Card>
+        {/* <ul>
+          <li><Link to={routes.TASKS}><span>Tasks</span></Link></li>
+          <li><Link to={routes.REVIEWS}><span>Reviews</span></Link></li>
+          <li><Link to={routes.REQUESTS}><span>Review requests</span></Link></li>
+        </ul> */}
+        <div style={{ width: 300 }}>
+          <Menu mode="inline">
+            <Menu.Item>
+              <Link to={routes.TASKS}><span>Tasks</span></Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to={routes.REVIEWS}><span>Reviews</span></Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to={routes.REQUESTS}><span>Review requests</span></Link>
+            </Menu.Item>
+          </Menu>
+        </div>
+
         <Button
           type="primary"
           onClick={() => {
@@ -44,7 +60,9 @@ const MainPage = ({ setIsAuthenticatedState, role }) => {
         >
           Log Out
         </Button>
-      </ul>
+      </div>
+      <div className="main-page-content-wrapper">Content</div>
+
     </div>
   );
 };
