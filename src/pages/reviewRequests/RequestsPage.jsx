28c8e7cfad16a13f7fac523f42205bb948fa7030
Alex <alex.gabriel@mail.ru>
2020-09-22 17:06:26 +0300

    feat: add fake data and columns for tables

--- src/pages/reviewRequests/RequestsPage.jsx
+++ src/pages/reviewRequests/RequestsPage.jsx
@@ -2,13 +2,186 @@ import React, { useState } from 'react';
 import { Table, Button } from 'antd';
 
 const RequestsPage = () => {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
+  const columns1 = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Chinese Score',
+      dataIndex: 'chinese',
+      sorter: {
+        compare: (a, b) => a.chinese - b.chinese,
+      },
+    },
+    {
+      title: 'Math Score',
+      dataIndex: 'math',
+      sorter: {
+        compare: (a, b) => a.math - b.math,
+      },
+    },
+    {
+      title: 'English Score',
+      dataIndex: 'english',
+      sorter: {
+        compare: (a, b) => a.english - b.english,
+      },
+    },
+  ];
+
+  const data1 = [
+    {
+      key: '1',
+      name: 'John Brown',
+      chinese: 98,
+      math: 60,
+      english: 70,
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      chinese: 98,
+      math: 66,
+      english: 89,
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      chinese: 98,
+      math: 90,
+      english: 70,
+    },
+    {
+      key: '4',
+      name: 'Jim Red',
+      chinese: 88,
+      math: 99,
+      english: 89,
+    },
+    {
+      key: '5',
+      name: 'John Brown',
+      chinese: 98,
+      math: 60,
+      english: 70,
+    },
+    {
+      key: '6',
+      name: 'Jim Green',
+      chinese: 98,
+      math: 66,
+      english: 89,
+    },
+    {
+      key: '7',
+      name: 'Joe Black',
+      chinese: 98,
+      math: 90,
+      english: 70,
+    },
+    {
+      key: '8',
+      name: 'Jim Red',
+      chinese: 88,
+      math: 99,
+      english: 89,
+    },
+    {
+      key: '9',
+      name: 'John Brown',
+      chinese: 98,
+      math: 60,
+      english: 70,
+    },
+    {
+      key: '10',
+      name: 'Jim Green',
+      chinese: 98,
+      math: 66,
+      english: 89,
+    },
+    {
+      key: '11',
+      name: 'Joe Black',
+      chinese: 98,
+      math: 90,
+      english: 70,
+    },
+    {
+      key: '12',
+      name: 'Jim Red',
+      chinese: 88,
+      math: 99,
+      english: 89,
+    },
+  ];
+  const columns2 = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Chinese Score',
+      dataIndex: 'chinese',
+      sorter: {
+        compare: (a, b) => a.chinese - b.chinese,
+      },
+    },
+    {
+      title: 'Math Score',
+      dataIndex: 'math',
+      sorter: {
+        compare: (a, b) => a.math - b.math,
+      },
+    },
+    {
+      title: 'English Score',
+      dataIndex: 'english',
+      sorter: {
+        compare: (a, b) => a.english - b.english,
+      },
+    },
+  ];
+
+  const data2 = [
+    {
+      key: '1',
+      name: 'John Brown',
+      chinese: 98,
+      math: 60,
+      english: 70,
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      chinese: 98,
+      math: 66,
+      english: 89,
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      chinese: 98,
+      math: 90,
+      english: 70,
+    },
+    {
+      key: '4',
+      name: 'Jim Red',
+      chinese: 88,
+      math: 99,
+      english: 89,
+    },
+  ];
+
   return (
     <div className="request-page-wrapper">
-      <Table />
-      <Table />
+      <Table columns={columns1} dataSource={data1} style={{ width: 700 }} />
+      <Table columns={columns2} dataSource={data2} style={{ width: 700 }} />
     </div>
   );
 };
 
-export default requestsPage;
+export default RequestsPage;
